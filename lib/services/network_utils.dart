9401474c1b2c43cd9ba1627b0620e1a1986823d6
Amansolo9 <amana67286535@gmail.com>
2024-12-27 21:56:10 +0300

    improved network ip configurator function and improved onboarding view

--- lib/services/network_utils.dart
+++ lib/services/network_utils.dart
@@ -1,12 +1,3 @@
-import 'dart:io';
-
 Future<String> getLocalIpAddress() async {
-  for (var interface in await NetworkInterface.list()) {
-    for (var addr in interface.addresses) {
-      if (addr.type == InternetAddressType.IPv4 && !addr.isLoopback) {
-        return addr.address;
-      }
-    }
-  }
-  return '127.0.0.1';
+  return '192.168.1.5';
 }
--- lib/views/splash/onboarding_view.dart
+++ lib/views/splash/onboarding_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
 class OnBoardingPage extends StatefulWidget {
@@ -13,85 +14,94 @@ class OnBoardingPageState extends State<OnBoardingPage> {
   final introKey = GlobalKey<IntroductionScreenState>();
 
   @override
+  void initState() {
+    super.initState();
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    const bodyStyle = TextStyle(fontSize: 19.0);
+    const bodyStyle = TextStyle(color: Color(0xFF9B1DFF), fontFamily: 'Poppins', fontSize: 19.0);
 
     const pageDecoration = PageDecoration(
-      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      titleTextStyle: TextStyle(color: Color(0xFF6600B7), fontFamily: 'Poppins', fontSize: 28.0, fontWeight: FontWeight.w700),
       bodyTextStyle: bodyStyle,
       bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-      pageColor: Colors.white,
+      pageColor: Color(0xFFF5EAFB),
       imagePadding: EdgeInsets.zero,
     );
 
-    return SafeArea(
-      child: IntroductionScreen(
-        key: introKey,
-        globalBackgroundColor: Colors.white,
-        allowImplicitScrolling: true,
-        autoScrollDuration: 3000,
-        infiniteAutoScroll: true,
-        globalHeader: Align(
-          alignment: Alignment.topRight,
-          child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 30, right: 16),
-              child: _buildImage('icon.png', 50),
+    return Scaffold(
+      backgroundColor: Color(0xFFF5EAFB),
+      body: SafeArea(
+        child: IntroductionScreen(
+          key: introKey,
+          globalBackgroundColor: Colors.transparent,
+          allowImplicitScrolling: true,
+          autoScrollDuration: 3000,
+          infiniteAutoScroll: true,
+          globalHeader: Align(
+            alignment: Alignment.topRight,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 30, right: 16),
+                child: _buildImage('icon.png', 50),
+              ),
             ),
           ),
-        ),
-
-        pages: [
-          PageViewModel(
-            title: "Welcome to Prico!",
-            body:
-                "Your go-to platform for comparing prices across multiple stores, ensuring you always get the best deal.",
-            image: _buildImage('hero1.jpg'),
-            decoration: pageDecoration,
+          pages: [
+            PageViewModel(
+              title: "Welcome to Prico!",
+              body:
+                  "Your go-to platform for comparing prices across multiple stores, ensuring you always get the best deal.",
+              image: _buildImage('hero1.png'),
+              decoration: pageDecoration,
+            ),
+            PageViewModel(
+              title: "Easily Compare Prices",
+              body:
+                  "Simply search for any product, and Prico will show you the best prices from different online retailers.",
+              image: _buildImage('hero2.png'),
+              decoration: pageDecoration,
+            ),
+            PageViewModel(
+              title: "Save Big on Every Purchase!",
+              body:
+                  "With Prico, you never have to overpay again. Find the lowest prices, access discounts, and make smart purchases.",
+              image: _buildImage('hero3.png'),
+              decoration: pageDecoration,
+            ),
+          ],
+          onDone: () => _onIntroEnd(context),
+          onSkip: () => _onIntroEnd(context),
+          showSkipButton: true,
+          skipOrBackFlex: 0,
+          nextFlex: 0,
+          showBackButton: false,
+          back: const Icon(Icons.arrow_back),
+          skip: const Text('Skip', style: TextStyle(color: Color(0xFF6600B7),fontFamily: 'Poppins', fontWeight: FontWeight.w600)),
+          next: const Icon(Icons.arrow_forward),
+          done: const Text('Done', style: TextStyle(color: Color(0xFF6600B7),fontFamily: 'Poppins', fontWeight: FontWeight.w600)),
+          curve: Curves.fastLinearToSlowEaseIn,
+          controlsMargin: const EdgeInsets.all(16),
+          controlsPadding: kIsWeb
+              ? const EdgeInsets.all(12.0)
+              : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+          dotsDecorator: const DotsDecorator(
+            size: Size(10.0, 10.0),
+            color: Color.fromRGBO(189, 189, 189, 1),
+            activeSize: Size(22.0, 10.0),
+            activeShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(25.0)),
+            ),
           ),
-          PageViewModel(
-            title: "Easily Compare Prices",
-            body:
-                "Simply search for any product, and Prico will show you the best prices from different online retailers.",
-            image: _buildImage('hero2.jpg'),
-            decoration: pageDecoration,
-          ),
-          PageViewModel(
-            title: "Save Big on Every Purchase!",
-            body:
-                "With Prico, you never have to overpay again. Find the lowest prices, access discounts, and make smart purchases.",
-            image: _buildImage('hero3.jpg'),
-            decoration: pageDecoration,
-          ),
-        ],
-        onDone: () => _onIntroEnd(context),
-        onSkip: () => _onIntroEnd(context), // You can override onSkip callback
-        showSkipButton: true,
-        skipOrBackFlex: 0,
-        nextFlex: 0,
-        showBackButton: false,
-        //rtl: true, // Display as right-to-left
-        back: const Icon(Icons.arrow_back),
-        skip: const Text('Skip', style: TextStyle(fontWeight: FontWeight.w600)),
-        next: const Icon(Icons.arrow_forward),
-        done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
-        curve: Curves.fastLinearToSlowEaseIn,
-        controlsMargin: const EdgeInsets.all(16),
-        controlsPadding: kIsWeb
-            ? const EdgeInsets.all(12.0)
-            : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
-        dotsDecorator: const DotsDecorator(
-          size: Size(10.0, 10.0),
-          color: Color.fromRGBO(189, 189, 189, 1),
-          activeSize: Size(22.0, 10.0),
-          activeShape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(25.0)),
-          ),
-        ),
-        dotsContainerDecorator: const ShapeDecoration(
-          color: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          dotsContainerDecorator: const ShapeDecoration(
+            color: Color(0xFFF5EAFB),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            ),
           ),
         ),
       ),
